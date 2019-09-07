231265db1c36adc6faf86f54c9f06329a08e0ed2
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2019-09-07 18:40:11 -0400

    Initial hacked-together websubmit
    Using rocket + Noria

---
+++ src/backend.rs
@@ -0,0 +1,61 @@
+use std::collections::BTreeMap;
+
+pub use noria::DataType;
+use noria::{SyncControllerHandle, SyncTable, SyncView, ZookeeperAuthority};
+
+pub struct NoriaBackend {
+    pub handle: SyncControllerHandle<ZookeeperAuthority, tokio::runtime::TaskExecutor>,
+    _log: slog::Logger,
+
+    _recipe: String,
+    pub tables: BTreeMap<String, SyncTable>,
+    pub views: BTreeMap<String, SyncView>,
+}
+
+impl NoriaBackend {
+    pub fn new(zk_addr: &str, log: Option<slog::Logger>) -> Self {
+        let log = match log {
+            None => slog::Logger::root(slog::Discard, o!()),
+            Some(l) => l,
+        };
+
+        let recipe = "CREATE TABLE answers (user varchar(255), lec int, q int, answer text, PRIMARY KEY (user));
+                      QUERY answers_by_lec: SELECT * FROM answers WHERE lec = ?;";
+
+        debug!(log, "Finding Noria via Zookeeper...");
+
+        let zk_auth = ZookeeperAuthority::new(&format!("{}", zk_addr))
+            .expect("failed to connect to Zookeeper");
+
+        debug!(log, "Connecting to Noria...");
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let executor = rt.executor();
+        let mut ch = SyncControllerHandle::new(zk_auth, executor)
+            .expect("failed to connect to Noria controller");
+
+        debug!(log, "Installing recipe in Noria...");
+        ch.install_recipe(&recipe.to_owned()).unwrap();
+
+        let inputs = ch
+            .inputs()
+            .expect("couldn't get inputs from Noria")
+            .into_iter()
+            .map(|(n, _)| (n.clone(), ch.table(&n).unwrap().into_sync()))
+            .collect::<BTreeMap<String, SyncTable>>();
+        let outputs = ch
+            .outputs()
+            .expect("couldn't get outputs from Noria")
+            .into_iter()
+            .map(|(n, _)| (n.clone(), ch.view(&n).unwrap().into_sync()))
+            .collect::<BTreeMap<String, SyncView>>();
+
+        NoriaBackend {
+            handle: ch,
+            _log: log,
+
+            _recipe: recipe.to_owned(),
+            tables: inputs,
+            views: outputs,
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,92 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate slog;
+extern crate slog_term;
+
+mod backend;
+
+use backend::{DataType, NoriaBackend};
+use rocket::request::Form;
+use rocket::response::NamedFile;
+use rocket::State;
+use std::io;
+use std::sync::{Arc, Mutex};
+
+pub fn new_logger() -> slog::Logger {
+    use slog::Drain;
+    use slog::Logger;
+    use slog_term::term_full;
+    Logger::root(Mutex::new(term_full()).fuse(), o!())
+}
+
+#[derive(Debug, FromForm)]
+struct FormInput {
+    q1: String,
+    sq1: String,
+}
+
+#[get("/<num>")]
+fn answers(num: u8, backend: State<Arc<Mutex<NoriaBackend>>>) -> String {
+    let mut bg = backend.lock().unwrap();
+    let mut h = bg.handle.view("answers_by_lec").unwrap().into_sync();
+
+    let key: DataType = (num as u64).into();
+
+    let res = h.lookup(&[key], true);
+
+    format!("{:?}", res)
+}
+
+#[get("/<num>")]
+fn questions(num: u8) -> io::Result<NamedFile> {
+    NamedFile::open(format!("static/m{}.html", num))
+}
+
+#[post("/<num>", data = "<data>")]
+fn questions_submit(
+    num: u8,
+    data: Form<FormInput>,
+    backend: State<Arc<Mutex<NoriaBackend>>>,
+) -> String {
+    let mut bg = backend.lock().unwrap();
+
+    let num: DataType = (num as u64).into();
+
+    let q1: Vec<DataType> = vec![
+        "malte".into(),
+        num.clone(),
+        1.into(),
+        data.q1.clone().into(),
+    ];
+    let sq1: Vec<DataType> = vec!["malte".into(), num, 2.into(), data.sq1.clone().into()];
+
+    let mut table = bg.handle.table("answers").unwrap().into_sync();
+
+    let res = table.insert(q1);
+    let res2 = table.insert(sq1);
+
+    format!("submitted: {:?} {:?}", res, res2)
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, CSCI 2390!"
+}
+
+fn main() {
+    let b = Arc::new(Mutex::new(NoriaBackend::new(
+        "127.0.0.1:2181/test",
+        Some(new_logger()),
+    )));
+
+    rocket::ignite()
+        .manage(b)
+        .mount("/", routes![index])
+        .mount("/questions", routes![questions, questions_submit])
+        .mount("/answers", routes![answers])
+        .launch();
+}
