3ce07bfa10b107991fb809e6e49d07ec2f3b649c
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2019-09-24 14:22:25 -0400

    Noria schema file

---
+++ src/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (email varchar(255), apikey text, PRIMARY KEY (apikey));
+CREATE TABLE lectures (id int, label varchar(255), qnum int, PRIMARY KEY (id));
+CREATE TABLE questions (lec int, q int, question text, PRIMARY KEY (lec, q));
+CREATE TABLE answers (user varchar(255), lec int, q int, answer text, PRIMARY KEY (user, lec, q));
+
+QUERY lecture: SELECT * FROM lectures WHERE id = ?;
+QUERY qs_by_lec: SELECT * FROM questions WHERE lec = ?;
+QUERY qnum_by_lec: SELECT id, qnum FROM lectures WHERE id = ?;
+QUERY answers_by_lec: SELECT * FROM answers WHERE lec = ?;
+QUERY users_by_apikey: SELECT * FROM users WHERE apikey = ?;
